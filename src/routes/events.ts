89a7e17a2fe749e6610061787f7866ed53c728e8
Miloš Ranđelović <milos@randelovic.me>
2023-07-24 19:41:48 +0700

    Added version to route paths

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -6,10 +6,10 @@ import express from "express";
 
 const eventRoutes = express.Router();
 
-eventRoutes.post("/events/jwt/:id", authenticationTypeAsyncHandler(postEvent, AuthenticationType.JWT));
-eventRoutes.all("/events/jwt/:id", authenticationTypeHandler(methodNotAllowedEvent, AuthenticationType.JWT));
+eventRoutes.post("/events/jwt/v1/:id", authenticationTypeAsyncHandler(postEvent, AuthenticationType.JWT));
+eventRoutes.all("/events/jwt/v1/:id", authenticationTypeHandler(methodNotAllowedEvent, AuthenticationType.JWT));
 
-eventRoutes.post("/events/key/:id", authenticationTypeAsyncHandler(postEvent, AuthenticationType.API_KEY));
-eventRoutes.options("/events/key/:id", authenticationTypeHandler(methodNotAllowedEvent, AuthenticationType.API_KEY));
+eventRoutes.post("/events/key/v1/:id", authenticationTypeAsyncHandler(postEvent, AuthenticationType.API_KEY));
+eventRoutes.options("/events/key/v1/:id", authenticationTypeHandler(methodNotAllowedEvent, AuthenticationType.API_KEY));
 
 export default eventRoutes;
